04a037f7f6597638e57039bee7cf0e24b0047127
WRXer <king_311@mail.ru>
2023-08-03 19:38:36 +0400

    В связанные привычки могут попадать только привычки с признаком приятной привычки.

--- main/models.py
+++ main/models.py
@@ -13,7 +13,7 @@ class Habit(models.Model):
     time = models.TimeField(verbose_name="время выполнения")   #время,когда выполнять
     action = models.CharField(max_length=200, verbose_name="действие")   #действие
     pleasant_habit = models.BooleanField(default=False, verbose_name="признак положительной привычки")     #признак положительной привычки
-    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True,limit_choices_to={'reward__isnull': False}, verbose_name="связанная привычка")     #связанная привычка
+    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True,limit_choices_to={'pleasant_habit': True}, verbose_name="связанная привычка")     #связанная привычка
     periodicity = models.PositiveIntegerField(default=1, verbose_name="периодичность")    #периодичность
     reward = models.CharField(max_length=200, verbose_name="вознаграждение", blank=True, null=True)   #вознаграждение
     time_to_complete = models.DurationField(verbose_name="время на выполнение")   #время на выполнение
