701d490c87a0a632af202ce90ffb806a8f1bd949
WRXer <king_311@mail.ru>
2023-08-03 21:56:50 +0400

    настроил селери и подготовил к отправке уведомлений в телегу

--- main/tasks.py
+++ main/tasks.py
@@ -1,18 +1,7 @@
 from celery import shared_task
 from django.conf import settings
-from telegram import Bot
-import json
 import requests
 
-
-@shared_task
-def send_telegram_message():
-    bot_token = settings.TELEGRAM_BOT_TOKEN  # Получите токен из настроек или из аргументов функции
-    bot = Bot(token=bot_token)
-    chat_id= 527186007
-    message = "Пора выполнить привычку:!"
-    bot.send_message(chat_id=chat_id, text=message)
-    print('cheeeck')
 
 
 @shared_task
